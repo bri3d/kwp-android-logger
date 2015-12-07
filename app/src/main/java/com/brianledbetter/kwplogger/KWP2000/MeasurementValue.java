98f2a9f6fa51d199fc59799b1b01ca61d8b4e341
Bri3D <brian@brianledbetter.com>
2015-12-07 10:54:58 -0700

    more parse types, passthrough raw values to display

--- app/src/main/java/com/brianledbetter/kwplogger/KWP2000/MeasurementValue.java
+++ app/src/main/java/com/brianledbetter/kwplogger/KWP2000/MeasurementValue.java
@@ -19,6 +19,20 @@ public class MeasurementValue {
                     return new MeasurementValue(Integer.toString(rpm), "rpm");
                 }
             });
+            put((byte)0x02, new MeasurementValueParser() {
+                @Override
+                public MeasurementValue parseBytes(byte[] bytes) {
+                    int percent = (bytes[0] * bytes[1]) / 500;
+                    return new MeasurementValue(Integer.toString(percent), "%");
+                }
+            });
+            put((byte)0x03, new MeasurementValueParser() {
+                @Override
+                public MeasurementValue parseBytes(byte[] bytes) {
+                    int deg = (bytes[0] * bytes[1]) / 500;
+                    return new MeasurementValue(Integer.toString(deg), "deg");
+                }
+            });
             put((byte) 0x04, new MeasurementValueParser() {
                 @Override
                 public MeasurementValue parseBytes(byte[] bytes) {
@@ -32,6 +46,13 @@ public class MeasurementValue {
                     }
                     int output = (Math.abs(bytes[1] - 127) * bytes[0])/100;
                     return new MeasurementValue(Integer.toString(output), unit);
+                }
+            });
+            put((byte) 0x05, new MeasurementValueParser() {
+                @Override
+                public MeasurementValue parseBytes(byte[] bytes) {
+                    int output = ((bytes[0]*bytes[1])/10) - (bytes[0] * 10);
+                    return new MeasurementValue(Integer.toString(output), "deg C");
                 }
             });
             put((byte) 0x06, new MeasurementValueParser() {
@@ -48,7 +69,32 @@ public class MeasurementValue {
                     return new MeasurementValue(Integer.toString(output), "km/h");
                 }
             });
-
+            put((byte) 0x08, new MeasurementValueParser() {
+                @Override
+                public MeasurementValue parseBytes(byte[] bytes) {
+                    int output = (bytes[0]*bytes[1])/10;
+                    return new MeasurementValue(Integer.toString(output), "-");
+                }
+            });
+            put((byte) 0x09, new MeasurementValueParser() {
+                @Override
+                public MeasurementValue parseBytes(byte[] bytes) {
+                    int output = ((bytes[1]-127)*bytes[0])/50;
+                    return new MeasurementValue(Integer.toString(output), "deg");
+                }
+            });
+            put((byte) 0x0A, new MeasurementValueParser() {
+                @Override
+                public MeasurementValue parseBytes(byte[] bytes) {
+                    String output;
+                    if(bytes[1] == 0) {
+                        output = "COLD";
+                    } else {
+                        output = "WARM";
+                    }
+                    return new MeasurementValue(output, "-");
+                }
+            });
             put((byte)0x1A, new MeasurementValueParser() {
                 @Override
                 public MeasurementValue parseBytes(byte[] bytes) {
@@ -65,7 +111,7 @@ public class MeasurementValue {
         if (mvp != null) {
             return mvp.parseBytes(Arrays.copyOfRange(byteValue, 1, 3));
         } else {
-            return new MeasurementValue("Error", "Error");
+            return new MeasurementValue(HexUtil.bytesToHexString(byteValue), "Raw");
         }
     }
 
