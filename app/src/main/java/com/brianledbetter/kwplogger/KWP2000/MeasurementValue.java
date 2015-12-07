4579bf15af70f7f6e5f8f119ca2c4db2e85a2660
Bri3D <brian@brianledbetter.com>
2015-12-07 11:50:30 -0700

    fix Double scaling, add more types

--- app/src/main/java/com/brianledbetter/kwplogger/KWP2000/MeasurementValue.java
+++ app/src/main/java/com/brianledbetter/kwplogger/KWP2000/MeasurementValue.java
@@ -15,22 +15,22 @@ public class MeasurementValue {
             put((byte)0x01, new MeasurementValueParser() {
                 @Override
                 public MeasurementValue parseBytes(byte[] bytes) {
-                    int rpm = (bytes[0] * bytes[1]) / 5;
-                    return new MeasurementValue(Integer.toString(rpm), "rpm");
+                    double rpm = (bytes[0] * bytes[1]) / 5.0;
+                    return new MeasurementValue(Double.toString(rpm), "rpm");
                 }
             });
             put((byte)0x02, new MeasurementValueParser() {
                 @Override
                 public MeasurementValue parseBytes(byte[] bytes) {
-                    int percent = (bytes[0] * bytes[1]) / 500;
-                    return new MeasurementValue(Integer.toString(percent), "%");
+                    double percent = (bytes[0] * bytes[1]) / 500.0;
+                    return new MeasurementValue(Double.toString(percent), "%");
                 }
             });
             put((byte)0x03, new MeasurementValueParser() {
                 @Override
                 public MeasurementValue parseBytes(byte[] bytes) {
-                    int deg = (bytes[0] * bytes[1]) / 500;
-                    return new MeasurementValue(Integer.toString(deg), "deg");
+                    double deg = (bytes[0] * bytes[1]) / 500.0;
+                    return new MeasurementValue(Double.toString(deg), "deg");
                 }
             });
             put((byte) 0x04, new MeasurementValueParser() {
@@ -51,15 +51,15 @@ public class MeasurementValue {
             put((byte) 0x05, new MeasurementValueParser() {
                 @Override
                 public MeasurementValue parseBytes(byte[] bytes) {
-                    int output = ((bytes[0]*bytes[1])/10) - (bytes[0] * 10);
-                    return new MeasurementValue(Integer.toString(output), "deg C");
+                    double output = ((bytes[0]*bytes[1])/10.0) - (bytes[0] * 10.0);
+                    return new MeasurementValue(Double.toString(output), "deg C");
                 }
             });
             put((byte) 0x06, new MeasurementValueParser() {
                 @Override
                 public MeasurementValue parseBytes(byte[] bytes) {
-                    int output = (bytes[0]*bytes[1])/1000;
-                    return new MeasurementValue(Integer.toString(output), "V");
+                    double output = (bytes[0]*bytes[1])/1000.0;
+                    return new MeasurementValue(Double.toString(output), "V");
                 }
             });
             put((byte) 0x07, new MeasurementValueParser() {
@@ -72,15 +72,15 @@ public class MeasurementValue {
             put((byte) 0x08, new MeasurementValueParser() {
                 @Override
                 public MeasurementValue parseBytes(byte[] bytes) {
-                    int output = (bytes[0]*bytes[1])/10;
-                    return new MeasurementValue(Integer.toString(output), "-");
+                    double output = (bytes[0]*bytes[1])/10.0;
+                    return new MeasurementValue(Double.toString(output), "-");
                 }
             });
             put((byte) 0x09, new MeasurementValueParser() {
                 @Override
                 public MeasurementValue parseBytes(byte[] bytes) {
-                    int output = ((bytes[1]-127)*bytes[0])/50;
-                    return new MeasurementValue(Integer.toString(output), "deg");
+                    double output = ((bytes[1]-127)*bytes[0])/50.0;
+                    return new MeasurementValue(Double.toString(output), "deg");
                 }
             });
             put((byte) 0x0A, new MeasurementValueParser() {
@@ -95,14 +95,133 @@ public class MeasurementValue {
                     return new MeasurementValue(output, "-");
                 }
             });
+            put((byte)0x0B, new MeasurementValueParser() {
+                @Override
+                public MeasurementValue parseBytes(byte[] bytes) {
+                    double out = ((bytes[1] - 128) * bytes[0] / 10000.0) + 1;
+                    return new MeasurementValue(Double.toString(out), "-");
+                }
+            });
+            put((byte)0x0C, new MeasurementValueParser() {
+                @Override
+                public MeasurementValue parseBytes(byte[] bytes) {
+                    double out = (bytes[0] * bytes[1]) / 1000.0;
+                    return new MeasurementValue(Double.toString(out), "ohm");
+                }
+            });
+            put((byte)0x0D, new MeasurementValueParser() {
+                @Override
+                public MeasurementValue parseBytes(byte[] bytes) {
+                    double out = ((bytes[1] - 127) * bytes[0] / 1000.0);
+                    return new MeasurementValue(Double.toString(out), "mm");
+                }
+            });
+            put((byte)0x0E, new MeasurementValueParser() {
+                @Override
+                public MeasurementValue parseBytes(byte[] bytes) {
+                    double out = (bytes[0] * bytes[1]) / 200.0;
+                    return new MeasurementValue(Double.toString(out), "bar");
+                }
+            });
+            put((byte)0x0F, new MeasurementValueParser() {
+                @Override
+                public MeasurementValue parseBytes(byte[] bytes) {
+                    double out = (bytes[0] * bytes[1]) / 100.0;
+                    return new MeasurementValue(Double.toString(out), "ms");
+                }
+            });
+            put((byte)0x10, new MeasurementValueParser() {
+                @Override
+                public MeasurementValue parseBytes(byte[] bytes) {
+                    int out = bytes[1] & bytes[0];
+                    return new MeasurementValue(Integer.toBinaryString(out), "binary");
+                }
+            });
+            put((byte)0x11, new MeasurementValueParser() {
+                @Override
+                public MeasurementValue parseBytes(byte[] bytes) {
+                    String out = bytes[0] + " " + bytes[1];
+                    return new MeasurementValue(out, "string");
+                }
+            });
+            put((byte)0x12, new MeasurementValueParser() {
+                @Override
+                public MeasurementValue parseBytes(byte[] bytes) {
+                    double out = .04 * bytes[0] * bytes[1];
+                    return new MeasurementValue(Double.toString(out), "mbar");
+                }
+            });
+            put((byte)0x13, new MeasurementValueParser() {
+                @Override
+                public MeasurementValue parseBytes(byte[] bytes) {
+                    double out = bytes[0] * bytes[1] / 100.0;
+                    return new MeasurementValue(Double.toString(out), "L");
+                }
+            });
+            put((byte)0x14, new MeasurementValueParser() {
+                @Override
+                public MeasurementValue parseBytes(byte[] bytes) {
+                    double out = bytes[0] * (bytes[1] - 128) / 128.0;
+                    return new MeasurementValue(Double.toString(out), "%");
+                }
+            });
+            put((byte)0x15, new MeasurementValueParser() {
+                @Override
+                public MeasurementValue parseBytes(byte[] bytes) {
+                    double out = bytes[0] * bytes[1] / 1000.0;
+                    return new MeasurementValue(Double.toString(out), "V");
+                }
+            });
+            put((byte)0x16, new MeasurementValueParser() {
+                @Override
+                public MeasurementValue parseBytes(byte[] bytes) {
+                    double out = bytes[0] * bytes[1] / 1000.0;
+                    return new MeasurementValue(Double.toString(out), "ms");
+                }
+            });
+            put((byte)0x17, new MeasurementValueParser() {
+                @Override
+                public MeasurementValue parseBytes(byte[] bytes) {
+                    double out = bytes[1] / 256.0 / bytes[0];
+                    return new MeasurementValue(Double.toString(out), "%");
+                }
+            });
+            put((byte)0x18, new MeasurementValueParser() {
+                @Override
+                public MeasurementValue parseBytes(byte[] bytes) {
+                    double out = bytes[0] * bytes[1] / 1000.0;
+                    return new MeasurementValue(Double.toString(out), "A");
+                }
+            });
+            put((byte)0x19, new MeasurementValueParser() {
+                @Override
+                public MeasurementValue parseBytes(byte[] bytes) {
+                    double out = (bytes[1] * 256 + bytes[1]) / 182.0;
+                    return new MeasurementValue(Double.toString(out), "g/sec");
+                }
+            });
             put((byte)0x1A, new MeasurementValueParser() {
                 @Override
                 public MeasurementValue parseBytes(byte[] bytes) {
                     int temperatureDegreesC = bytes[1] - bytes[0];
-                    return new MeasurementValue(Integer.toString(temperatureDegreesC), "degC");
+                    return new MeasurementValue(Integer.toString(temperatureDegreesC), "deg C");
                 }
             });
+            put((byte) 0x1B, new MeasurementValueParser() {
+                @Override
+                public MeasurementValue parseBytes(byte[] bytes) {
+                    String unit;
+                    if (bytes[1] > 128) {
 
+                        unit = "deg BTDC";
+                    }
+                    else {
+                        unit = "deg ATDC";
+                    }
+                    int output = (Math.abs(bytes[1] - 128) * bytes[0])/100;
+                    return new MeasurementValue(Integer.toString(output), unit);
+                }
+            });
         }
     };
 
