87a4ae690c6443ac14989ae028351b0957cbf49e
Bri3D <brian@brianledbetter.com>
2015-12-12 20:27:37 -0700

    add integration testing IO and integration test for login

--- app/src/androidTest/java/com/brianledbetter/kwplogger/KWP2000TestIO.java
+++ app/src/androidTest/java/com/brianledbetter/kwplogger/KWP2000TestIO.java
@@ -1,7 +1,47 @@
 package com.brianledbetter.kwplogger;
 
+import com.brianledbetter.kwplogger.KWP2000.HexUtil;
+import com.brianledbetter.kwplogger.KWP2000.KWP2000IO;
+import com.brianledbetter.kwplogger.KWP2000.KWPException;
+
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by b3d on 12/12/15.
  */
-public class KWP2000TestIO {
+public class KWP2000TestIO implements KWP2000IO {
+    private List<byte[]> m_readBytes;
+    private List<byte[]> m_expectedWriteBytes;
+
+    public KWP2000TestIO(List<byte[]> readBytes, List<byte[]> expectedWriteBytes) {
+        m_readBytes = readBytes;
+        m_expectedWriteBytes = expectedWriteBytes;
+    }
+
+    @Override
+    public byte[] readBytes() throws KWPException {
+        if (m_readBytes.size() == 0) {
+            throw new KWPException("Trying to read bytes but none available!");
+        }
+        return m_readBytes.remove(0);
+    }
+
+    @Override
+    public void writeBytes(byte[] bytesToWrite) throws KWPException {
+        if(m_expectedWriteBytes.size() == 0) {
+            throw new KWPException("Wrote more bytes than expected!");
+        }
+        byte[] expectedBytes = m_expectedWriteBytes.remove(0);
+        if (!Arrays.equals(expectedBytes, bytesToWrite)) {
+            throw new KWPException("Expected we were going to write " + HexUtil.bytesToHexString(expectedBytes) + " but wrote " + HexUtil.bytesToHexString(bytesToWrite));
+        }
+    }
+
+    @Override
+    public void startKWPIO(byte initAddress, byte controllerAddress) throws KWPException {
+
+    }
+
+
 }
---
+++ app/src/androidTest/java/com/brianledbetter/kwplogger/LoginIntegrationTest.java
@@ -0,0 +1,42 @@
+package com.brianledbetter.kwplogger;
+
+import com.brianledbetter.kwplogger.KWP2000.DiagnosticSession;
+import com.brianledbetter.kwplogger.KWP2000.KWP2000IO;
+import com.brianledbetter.kwplogger.KWP2000.KWPException;
+
+import junit.framework.TestCase;
+
+import java.util.ArrayList;
+
+/**
+ * Created by b3d on 12/12/15.
+ */
+public class LoginIntegrationTest extends TestCase {
+    private KWP2000IO m_testIO;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        ArrayList<byte[]> readBytes = new ArrayList<byte[]>();
+        ArrayList<byte[]> writeBytes = new ArrayList<byte[]>();
+        writeBytes.add(new byte[]{(byte)0x1A,(byte)0x92});
+        writeBytes.add(new byte[]{(byte)0x27,(byte)0x01});
+        writeBytes.add(new byte[] {(byte)0x27,(byte)0x02,(byte)0xB1,(byte)0x85,(byte)0xF9,(byte)0x6E});
+        readBytes.add(new byte[] {(byte)0x5A,(byte)0x92,(byte)0x30,(byte)0x32,(byte)0x36,(byte)0x31,(byte)0x53});
+        readBytes.add(new byte[] {(byte)0x67,(byte)0x01,(byte)0x01,(byte)0xAA,(byte)0x20,(byte)0xC4});
+        readBytes.add(new byte[] {(byte)0x62,(byte)0x02});
+        m_testIO = new KWP2000TestIO(readBytes, writeBytes);
+    }
+
+    public void testLogin() {
+        DiagnosticSession testSession = new DiagnosticSession(m_testIO);
+        try {
+            assertEquals(testSession.securityLogin(), true);
+        } catch (KWPException e) {
+            fail(e.toString());
+        }
+
+
+    }
+
+}
