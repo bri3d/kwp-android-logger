978e6c2348afa0d4fb7de82aa0f49e7968b00dd9
Bri3D <brian@brianledbetter.com>
2016-02-28 11:22:23 -0700

    move KWP2000 -> github.com/bri3d/kwp2000

--- kwp2000/src/main/java/com/brianledbetter/kwplogger/KWP2000/ECUIdentification.java
+++
@@ -1,9 +0,0 @@
-package com.brianledbetter.kwplogger.KWP2000;
-
-/**
- * Created by b3d on 12/5/15.
- */
-public class ECUIdentification {
-    public String softwareNumber;
-    public String hardwareNumber;
-}
--- kwp2000/src/main/java/com/brianledbetter/kwplogger/KWP2000/MeasurementValueParser.java
+++
@@ -1,8 +0,0 @@
-package com.brianledbetter.kwplogger.KWP2000;
-
-/**
- * Created by b3d on 12/6/15.
- */
-public interface MeasurementValueParser {
-    MeasurementValue parseBytes(int[] bytes); // we use int because of wonder, wonderful signed bytes
-}
